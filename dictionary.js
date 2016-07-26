ce3bfd0e0a5168e0f1d5f6d473b95a3951c92da8
Bryan Jennings <bryanjenningz@gmail.com>
2016-07-25 19:52:18 -0700

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,10 @@
+var dictionary = {
+  '你好': {pronunciation: 'ni3 hao3', translation: 'hello'},
+  '你': {pronunciation: 'ni3', translation: 'you'},
+  '我': {pronunciation: 'wo3', translation: 'me, I'},
+  '是': {pronunciation: 'shi4', translation: 'is, be'},
+  '布莱恩': {pronunciation: 'bu4 lai2 en1', translation: 'Brian, Bryan'},
+  '叫': {pronunciation: 'jiao4', translation: 'called'},
+  '什么': {pronunciation: 'shen2 me0', translation: 'what'},
+  '名字': {pronunciation: 'ming2 zi4', translation: 'name'}
+};
